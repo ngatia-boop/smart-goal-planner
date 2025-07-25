a2b1024b5a4989a66f03aa14c52a19d25b8bc515
ann ngatia <annngatia94@gmail.com>
2025-07-25 11:59:05 +0300

    modify api.js

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-const BASE_URL = "https://smart-goal-api.onrender.com";
+const BASE_URL = " https://smart-goal-api-1.onrender.com";
 
 export const fetchGoals = () => axios.get(BASE_URL);
 export const addGoal = (goal) => axios.post(BASE_URL, goal);
 export const updateGoal = (id, updates) => axios.patch(`${BASE_URL}/${id}`, updates);
 export const deleteGoal = (id) => axios.delete(`${BASE_URL}/${id}`);
+
